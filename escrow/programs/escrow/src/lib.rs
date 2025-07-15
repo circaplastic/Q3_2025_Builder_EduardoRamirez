77c15d3a3c2241c0fb32567541ad5e370a6602f3
EduardoRamirez <circaplastic@gmail.com>
2025-07-15 13:33:57 -0600

    Wk2Ex2 Escrow program

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -1,5 +1,16 @@
+/*Escrow program succesfully deployed to devnet
+Signature: 5cr9ptATxkg5TvnXJQEP3jYXvT3mZ6dZ2A5yBnSLp7idF3GzSiwpz3eF5YFrb11AUhyE2TW6d4mExfdo3RiWBzJb
+Deploy success
+*/
+
 #![allow(unexpected_cfgs, deprecated)]
 use anchor_lang::prelude::*;
+
+pub mod instructions;
+pub mod state;
+
+use instructions::*;
+use state::*;
 
 declare_id!("4JZUud5jkp1diBpkVqE1N7Ra62UEmyHYUhFqgbuT5bBZ");
 
@@ -7,11 +18,16 @@ declare_id!("4JZUud5jkp1diBpkVqE1N7Ra62UEmyHYUhFqgbuT5bBZ");
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)?;
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)?;
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
